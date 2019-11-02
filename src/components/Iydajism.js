0d2deb9162bb89a8e260f9517f09a745a8a2374e
volo g <schamane.vzl@gmail.com>
2019-11-02 19:39:26 +0200

    update

--- src/components/Iydajism.js
+++ src/components/Iydajism.js
@@ -15,6 +15,18 @@ function Iydajism() {
           <li>книга - Танах</li>
          </ul>
       </div>
+
+      <div>
+        <ul>
+          <li><a href="https://www.youtube.com/watch?v=H98yj2Kr7lw">https://www.youtube.com/watch?v=H98yj2Kr7lw - ШКОЛА МИСЛЕННЯ Випуск 1 ЮДАЇЗМ - НІ, БОГОЯВЛЕННЯ І БОГОРОДИЧНІСТЬ - ТАК В ХРИСТИЯНСТВІ</a></li>
+          <li><a href="https://www.youtube.com/watch?v=J9biEgJcg5k">https://www.youtube.com/watch?v=J9biEgJcg5k - ШКОЛА МИСЛЕННЯ Випуск 14 ЮДАЇЗМ ч.1</a></li>
+          <li><a href="https://www.youtube.com/watch?v=PLifvfW8KvQ">https://www.youtube.com/watch?v=PLifvfW8KvQ - ШКОЛА МИСЛЕННЯ Випуск 15 ЮДАЇЗМ ч.2</a></li>
+          <li><a href="https://www.youtube.com/watch?v=0AS7t1Za4oA">https://www.youtube.com/watch?v=0AS7t1Za4oA - ШКОЛА МИСЛЕННЯ Випуск 16 ЮДАЇЗМ ч.3</a></li>
+          <li><a href="https://www.youtube.com/watch?v=_USS1IJMxN8">https://www.youtube.com/watch?v=_USS1IJMxN8 - ШКОЛА МИСЛЕННЯ Випуск 17 ЮДАЇЗМ ч.4</a></li>
+          <li><a href="https://www.youtube.com/watch?v=DeVn0TTywCM">https://www.youtube.com/watch?v=DeVn0TTywCM - ШКОЛА МИСЛЕННЯ Випуск 18 ЮДАЇЗМ ч.5</a></li>
+          <li><a href=""></a></li>
+        </ul>
+      </div>
     </>
   );
 };
